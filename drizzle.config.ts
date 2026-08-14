fcb2c78d15ddbe4891d788ab7430fc1f5eeb34e2
Nathnael <natisteph2010@gmail.com>
2026-08-14 03:19:56 +0300

    db fix 4

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   schema: "./db/schema.ts",
   out: "netlify/database/migrations",
   dbCredentials: {
-    url: "postgresql://postgres.obrkzsxeejvsppfjilfl:Natiman2010!@aws-0-eu-west-1.pooler.supabase.com:6543/postgres",
+    url: "postgresql://postgres:Natiman2010!@db.obrkzsxeejvsppfjilfl.supabase.co:5432/postgres",
   },
 });
